274a8fb89fce69c5a1c36046fa8558b4fa833de9
montonero <inmate66+gh@gmail.com>
2023-05-21 01:13:06 +0300

    Throw exceptions if registering the extension fails
    Add support for more than 15 selected items

--- intag/RegUtils.cs
+++ intag/RegUtils.cs
@@ -4,58 +4,54 @@ using Microsoft.Win32;
 
 namespace intag
 {
-	public static class RegUtils
-	{
-		private static void InstallExtension(string ext)
-		{
-			var subKeyIcon = Registry.ClassesRoot.CreateSubKey($@"{ext}\shell\{Constants.RegistryName}");
-			if (subKeyIcon == null)
-			{
-				return;
-			}
-			
-			var subKey = Registry.ClassesRoot.CreateSubKey($@"{ext}\shell\{Constants.RegistryName}\command");
-			if (subKey == null)
-			{
-				return;
-			}
-			
-			var loc = Assembly.GetEntryAssembly()?.Location;
-			if (loc == null)
-			{
-				subKey.Close();
-				Environment.Exit(1);
-			}
+    public static class RegUtils
+    {
+        private static void InstallExtension(string ext)
+        {
+            var subKey = Registry.ClassesRoot.CreateSubKey($@"{ext}\shell\{Constants.RegistryName}\command")
+                ?? throw new ApplicationException("Cannot create shell registry key");
+            var subKeyIcon = Registry.ClassesRoot.OpenSubKey($@"{ext}\shell\{Constants.RegistryName}", true)
+                ?? throw new ApplicationException("Cannot open shell registry key");
 
-			loc = $"\"{loc}\"";
-			
-			subKeyIcon.SetValue("Icon", loc);
-			subKeyIcon.Close();
-			subKey.SetValue("", loc+" \"%1\"");
-			subKey.Close();  
-		}
+            var loc = Assembly.GetEntryAssembly()?.Location;
+            if (loc == null)
+            {
+                subKey.Close();
+                subKeyIcon.Close();
 
-		private static void InstallFiles() => InstallExtension("*");
-		private static void InstallFolder() => InstallExtension("Folder");
-		
-		public static void Install()
-		{
-			InstallFolder();
-			InstallFiles();
-		}
+                throw new ApplicationException("Cannot get assembly's location");
+            }
 
-		private static void UninstallExt(string ext)
-		{
-			Registry.ClassesRoot.DeleteSubKeyTree($@"{ext}\shell\{Constants.RegistryName}", false);
-		}
+            loc = $"\"{loc}\"";
 
-		private static void UninstallFolder() => UninstallExt("Folder");
-		private static void UninstallFiles() => UninstallExt("*");
+            subKeyIcon.SetValue("Icon", loc);
+            subKeyIcon.SetValue("MultiSelectModel", "Player");
+            subKeyIcon.Close();
+            subKey.SetValue("", loc+" \"%1\"");
+            subKey.Close();
+        }
 
-		public static void Uninstall()
-		{
-			UninstallFolder();
-			UninstallFiles();
-		}
-	}
+        private static void InstallFiles() => InstallExtension("*");
+        private static void InstallFolder() => InstallExtension("Folder");
+
+        public static void Install()
+        {
+            InstallFolder();
+            InstallFiles();
+        }
+
+        private static void UninstallExt(string ext)
+        {
+            Registry.ClassesRoot.DeleteSubKeyTree($@"{ext}\shell\{Constants.RegistryName}", false);
+        }
+
+        private static void UninstallFolder() => UninstallExt("Folder");
+        private static void UninstallFiles() => UninstallExt("*");
+
+        public static void Uninstall()
+        {
+            UninstallFolder();
+            UninstallFiles();
+        }
+    }
 }
